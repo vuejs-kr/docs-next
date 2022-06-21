65de80af21bd8488666c6f4e2acbec2a68d0be62
niceplugin <niceplugin@gmail.com>
2022-06-21 23:53:59 +0900

    예제 번역: /examples/grid

--- ko-KR/src/examples/src/grid/App/options.js
+++ ko-KR/src/examples/src/grid/App/options.js
@@ -8,10 +8,10 @@ export default {
     searchQuery: '',
     gridColumns: ['name', 'power'],
     gridData: [
-      { name: 'Chuck Norris', power: Infinity },
-      { name: 'Bruce Lee', power: 9000 },
-      { name: 'Jackie Chan', power: 7000 },
-      { name: 'Jet Li', power: 8000 }
+      { name: '손오공', power: Infinity },
+      { name: '이소룡', power: 9000 },
+      { name: '성룡', power: 7000 },
+      { name: '이연걸', power: 8000 }
     ]
   })
 }
